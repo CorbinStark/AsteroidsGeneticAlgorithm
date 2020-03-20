224ed2258e8f03a6c6cc9065c44fc8ad5e844711
MQZON <bmo@pdx.edu>
2020-03-20 15:22:06 -0700

    cleaned up unused code

--- GA.py
+++ GA.py
@@ -6,29 +6,17 @@ import QLearning as Q
 import constant as C
 import main
 
-#Tunable parameters
-PopulationSize = 10								#Number of chromosomes.
-NumIterations = 10  							#Number of generations.
-SimulationLength = 1000                         #Number of frames to simulate
-MutationPct = 0.45								#Liklihood of mutation.
-Replacement = True								#Multiple recombination.
-Kickstart = True								#Start with unique rows.
-Elitism = True									#Better parents persist.
-Resets = True									#Allow reset when stale.
-StaleFactor = 0.05								#Staleness before reset.
-statespace = len(C.results)**len(C.sensors)
-
 def random_chromosome():
-    return [random.randint(1,(len(C.actions)))-1 for i in range(statespace)]
+    return [random.randint(1,(len(C.actions)))-1 for i in range(C.statespace)]
 
 def updateAction(player, chromosome):
     return C.actions[chromosome[C.state.index(player.state)]]
 
 def average_fitness(fitness_scores):
     total_fitness = 0
-    for each in range(PopulationSize):
+    for each in range(C.PopulationSize):
         total_fitness += fitness_scores[each]
-    return total_fitness/PopulationSize
+    return total_fitness/C.PopulationSize
 
 def selection_chance(fitness_scores, chromosome, remaining):
     this_fitness = fitness_scores[chromosome]
@@ -55,30 +43,30 @@ def select_pair(population, fitness_scores, remaining):
     if p2 == -1:
         if p1 == 0: p2 = 1
         else: p2 = 0
-    if Replacement: return [population[max(p1, p2)], population[min(p1, p2)]]
+    if C.Replacement: return [population[max(p1, p2)], population[min(p1, p2)]]
     else: return [population.pop(max(p1, p2)), population.pop(min(p1, p2))], population
 
 def breed(population, fitness_scores):
     newPopulation = []
     newFitnessScores = []
-    remaining = PopulationSize
+    remaining = C.PopulationSize
     while remaining > 0:
-        if Replacement: p = select_pair(population, fitness_scores, PopulationSize)
+        if C.Replacement: p = select_pair(population, fitness_scores, C.PopulationSize)
         else: p, population = select_pair(population, fitness_scores, remaining)
         p1 = p[0]
         p2 = p[1]
-        r = random.randrange(1, statespace)
+        r = random.randrange(1, C.statespace)
         c1 = p1[:r]+p2[r:]
         c2 = p2[:r]+p1[r:]
-        if random.random() < MutationPct:
-            g1 = random.randrange(statespace)
-            g2 = random.randrange(statespace)
+        if random.random() < C.MutationPct:
+            g1 = random.randrange(C.statespace)
+            g2 = random.randrange(C.statespace)
             c1[g1], c1[g2] = c1[g2], c1[g1]
-        if random.random() < MutationPct:
-            g1 = random.randrange(statespace)
-            g2 = random.randrange(statespace)
+        if random.random() < C.MutationPct:
+            g1 = random.randrange(C.statespace)
+            g2 = random.randrange(C.statespace)
             c2[g1], c2[g2] = c2[g2], c2[g1]
-        if Elitism:
+        if C.Elitism:
             p1score = fitness_scores[population.index(p[0])]
             p2score = fitness_scores[population.index(p[1])]
             c1score = main.simulate(main.newGameContainer(), c1)
@@ -101,76 +89,3 @@ def breed(population, fitness_scores):
 
 def best_solution(fitness_scores):
     return fitness_scores.index(max(fitness_scores))
-
-
-
-
-#calculate the fitness of a chromosome
-def fitness(chromosome):
-    return int(maxFitness - score())
-
-#our probability is just defined by the ratio of a chromosomes fitness compared to our maximum fitness threshold
-def probability(chromosome, maxFitness):
-    return fitness(chromosome)/ maxFitness
-#randomly pick nodes within a certain range.
-def random_pick(population, probabilities):
-    population_with_probability = zip(population, probabilities)
-    total = sum(w for c, w in population_with_probability)
-    r = random.uniform(0,total)
-    upto = 0
-    for c, w in zip(population, probabilities):
-        if upto + w >= r:
-            return c
-        upto += w
-    assert False, "Error, something went wrong......"
-#combine the first trait of the x "genome" with the y "genome"
-def reproduce(x,y):
-    n = len(x)
-    c = random.randint(0, n-1)
-    return x[0:c] + y[c:n]
-#randomly changes an aspect about an unfortunate child node
-def mutate(x):
-    n = len(x)
-    c = random.randint(0, n-1)
-    m = random.randint(1,n)
-    x[c] = m
-    return x
-#this is where the actual magic happens, all the functions above were helper functions for this
-# we set our mutation here and this is where we modify our population
-def genetic_algorithm(iterations, maxIterations, population, fitness):
-    iterations += 1
-    mutation_probability = 0.055
-    new_population = []
-    probabilities = [probability(n, fitness) for n in population]
-    #after we find our probabilities for our nodes, then start to select their attributes to reproduce with
-    for i in range(len(population)):
-        x = random_pick(population, probabilities)
-        y = random_pick(population, probabilities)
-        child = reproduce(x,y)
-        #if our mutation is greater than our randomly generated number, mutate a part of the child
-        if random.random() < mutation_probability:
-            child = mutate(child)
-        print_chromosome(child)
-        #add this new child to the generation
-        new_population.append(child)
-        #we found our solution child! Or we reached our limit
-        if fitness(child) == maxFitness: break
-        if iterations == maxIterations: break
-    return new_population
-#prints individual information about a chromosome
-def print_chromosome(chrom):
-    print("Chromosome = {}, Fitness = {}".format(str(chrom), fitness(chrom)))
-#
-#def main():
-#     #these three variables are placeholders
-#    maxFitness = (timeStepCount *asteroidPoints * asteroidAmount)
-#    maxIterations = 500
-#    iterations = 0
-#    population = [random_chromosome() for _ in range(100)]
-#    while not maxFitness in [fitness(chrom) for chrom in population]:
-#        population = genetic_algorithm(iterations, maxIterations, population, fitness)
-#        iterations += 1
-
-
-#if __name__ == "__main__":
-#    main()
--- QLearning.py
+++ QLearning.py
@@ -1,9 +1,6 @@
 import random
 import constant as C
 
-#episodes = 5000
-#lifespan = 200
-takerisk = 0.1
 Q_Matrix = []
 
 def initialize():
@@ -11,47 +8,10 @@ def initialize():
       return Q_Matrix
 
 def choose_action(state):
-    if random.random() < takerisk: return random.choice(range(len(C.actions)))
+    if random.random() < C.takerisk: return random.choice(range(len(C.actions)))
     else: return greedy_choice(state)
 
 def greedy_choice(state):
     best = max(Q_Matrix[C.state.index(state)])
     bests = [i for i, x in enumerate(Q_Matrix[C.state.index(state)]) if x == best]
     return random.choice(bests)
-
-def act(action, player):
-    initscore = player.score
-    finalscore = initscore
-    #execute given action
-    #TODO figure this stuff out
-    if action == 'Left':
-        player.position = ((player.position + MOVESPEED) * FRAMES_PER_ACTION) * moveVector
-        updatePosition(player)
-        drawPlayer(player, ship, win)
-    if action == 'Right':
-        player.position = ((player.position + MOVESPEED) * FRAMES_PER_ACTION) * moveVector
-        updatePosition(player)
-        drawPlayer(player, ship, win)
-    if action == 'Thrust':
-        player.position = ((player.position + MOVESPEED) * FRAMES_PER_ACTION) * moveVector
-        updatePosition(player)
-        drawPlayer(player,ship,win)
-    if action == 'Shoot':
-        drawPlayer(player, ship,win)
-        fireProjectile(player, ship)
-        drawProjectiles(projectiles, win)
-        if(detectProjectileCollision(asteroids, projectiles)):
-            finalscore += asteroidValue
-
-def train(player, Q_Matrix):
-    initstate = C.state.index(player.state)
-    action = choose_action(player.state)
-    prev = Q_Matrix[initstate][action]
-    reward = act(C.actions[action], player)
-    nextbest = Q_Matrix[C.state.index(player.state)][greedy_choice(player.state)]
-    Q_Matrix[initstate][action] = prev + stepsize*(reward + discount*nextbest - prev)
-    return Q_Matrix
-
-def step(player):
-    action = choose_action(player.state)
-    reward = act(C.actions[action], player)
